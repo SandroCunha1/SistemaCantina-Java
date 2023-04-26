7ec12ae7f40ba266127252ea92464a62e733ff30
Sandro <sandromedeirosdacunha@gmail.com>
2023-04-26 19:07:06 -0300

    Primeiro commit, já adicionado todas as classes do diagrama de classes no Model.

---
+++ src/model/bo/Bairro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Bairro {
+    private int id;
+    private String descricao;
+
+    public Bairro(){   
+    }
+    public Bairro(int id, String descricao){
+        this.id= id;
+        this.descricao = descricao;
+    }
+    
+    
+    
+    public int getId(){
+        return id;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+    
+}
